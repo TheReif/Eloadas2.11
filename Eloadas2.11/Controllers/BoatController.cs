c63ac639b11bf678760fee8db3f2c1922cadc957
TheReif <reifmarton@gmail.com>
2022-11-30 14:10:34 +0100

    Add project files.

---
+++ Eloadas2.11/Controllers/BoatController.cs
@@ -0,0 +1,37 @@
+﻿using Eloadas2._11.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eloadas2._11.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("hajo/kerdesek")]
+        public IActionResult fv1()
+        {
+            HajosContext hajosContext = new Models.HajosContext();
+            var lisa = from x in hajosContext.Questions
+                       select x;
+            return Ok(lisa);
+        }
+
+        [HttpGet]
+        [Route("hajo/kerdesek/{id}")]
+        public IActionResult fv2(int id)
+        {
+            HajosContext hajosContext = new HajosContext();
+            var lisa = from x in hajosContext.Questions
+                       where x.QuestionId == id
+                       select x;
+           
+
+
+            var lisa2 = hajosContext.Questions.Where(x => x.QuestionId == id);
+            return Ok(lisa.FirstOrDefault());
+        }
+    }
+}
---
+++ Eloadas2.11/Controllers/test.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security;
+
+namespace Eloadas2._11.Controllers
+{
+    /*[Route("api/[controller]")]
+    [ApiController]*/
+    public class test : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult mind1()
+        {
+            return Ok(DateTime.Now.ToString());
+        }
+
+        [HttpGet]
+        [Route("corvinus/echo/{id}")]
+        public IActionResult mind2(string id)
+        {
+            return Ok(id.ToUpper().Trim());
+        }
+    }
+}
